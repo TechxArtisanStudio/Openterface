f30717f55101bb7d1a0c65ea2c61415f916a2d2c
BillyW_M2 <youyoubilly@gmail.com>
2024-01-19 10:55:22 +0800

    update to 0.0.0.0:8002

--- run_local.py
+++ run_local.py
@@ -3,7 +3,7 @@ import subprocess
 def run_mkdocs():
     try:
         # Running the mkdocs serve command
-        subprocess.run(["mkdocs", "serve", "-a", "0.0.0.0:8001"])
+        subprocess.run(["mkdocs", "serve", "-a", "0.0.0.0:8002"])
     except Exception as e:
         print(f"An error occurred: {e}")
 
