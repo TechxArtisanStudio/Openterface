73bd4809c0ae4cf6c050d64fbf28c7cf7d58c5d6
BillyW_M2 <youyoubilly@gmail.com>
2024-01-25 11:59:18 +0800

    add loadingMessage for tweets loading

--- docs/assets/javascripts/custom.js
+++ docs/assets/javascripts/custom.js
@@ -1,15 +1,29 @@
 window.onload = function() {
+    // Hide the loading message when tweets are loaded
+    var hideLoadingMessage = function() {
+        var loadingMessage = document.getElementById('loadingMessage');
+        if (loadingMessage) {
+            loadingMessage.style.display = 'none';
+        }
+    };
+
     // Check if the Twitter widgets script is loaded
     if (typeof twttr !== 'undefined') {
-      twttr.widgets.load(
-        document.getElementById("twitter-feed")
-      );
-      twttr.events.bind('loaded', function (event) {
-        // Find all twitter tweets and show them
-        var tweets = document.querySelectorAll('.twitter-tweet');
-        tweets.forEach(function(tweet) {
-          tweet.classList.add('twitter-tweet-loaded');
+        twttr.widgets.load(
+            document.getElementById("twitter-feed")
+        );
+        twttr.events.bind('loaded', function (event) {
+            // Hide loading message
+            hideLoadingMessage();
+
+            // Find all twitter tweets and show them
+            var tweets = document.querySelectorAll('.twitter-tweet');
+            tweets.forEach(function(tweet) {
+                tweet.classList.add('twitter-tweet-loaded');
+            });
         });
-      });
+    } else {
+        // Hide loading message if Twitter script isn't found
+        hideLoadingMessage();
     }
 };
