0b5edac05c6701f8fc8e52453387b3d024fc8b64
Billy-C1 <billy@techxartisan.com>
2025-01-29 13:56:58 +0800

    update run scripts

--- run_local.py
+++ run_local.py
@@ -2,8 +2,13 @@ import subprocess
 
 def run_mkdocs():
     try:
-        # Running the mkdocs serve command
-        subprocess.run(["mkdocs", "serve", "-a", "0.0.0.0:8002"])
+        # Path to the Python binary in the virtual environment
+        python_path = "/home/project/openterface/website/mkdocs-venv/bin/python"
+        
+        # Run mkdocs using the Python binary in the virtual environment
+        subprocess.run([python_path, "-m", "mkdocs", "serve", "-a", "0.0.0.0:8002"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
