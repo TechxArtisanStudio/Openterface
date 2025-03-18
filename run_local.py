3126bfc9896fe8f8453dadf18dbe822731847cd5
Billy Wang <billy@techxartisan.com>
2025-03-18 10:55:31 +0800

    add git-committers

--- run_local.py
+++ run_local.py
@@ -1,12 +1,15 @@
 import subprocess
+import os
 
 def run_mkdocs():
     try:
-        # Path to the Python binary in the virtual environment
-        python_path = "openterface/bin/python"
+        # Activate the virtual environment
+        # venv_path = os.path.expanduser("<venv_path>/bin/activate")
+        # command = f"source {venv_path} && mkdocs serve -a 0.0.0.0:8002"
+        command = f"mkdocs serve -a 0.0.0.0:8002"
         
-        # Run mkdocs using the Python binary in the virtual environment
-        subprocess.run([python_path, "-m", "mkdocs", "serve", "-a", "0.0.0.0:8002"], check=True)
+        # Run mkdocs using the activated virtual environment
+        subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Command failed with error: {e}")
     except Exception as e:
